88aa2ffa122f01a32308bdf577be3b8626310080
Jin Hu <bixuehujin@gmail.com>
2018-04-05 23:33:53 +0800

    Added constants for http verbs

--- src/http/Request.php
+++ src/http/Request.php
@@ -28,6 +28,14 @@ class Request extends BaseObject implements ShouldBeRefreshed, ServerRequestInte
     use MiddlewareTrait;
     use MessageTrait;
 
+    const METHOD_HEAD = 'HEAD';
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_PATCH = 'PATCH';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_OPTIONS = 'OPTIONS';
+
     public $method = '';
 
     /**
