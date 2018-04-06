35b688bfb2f5009a0a8d30498606dc8df0ff56aa
Jin Hu <bixuehujin@gmail.com>
2018-04-06 19:11:59 +0800

    Bump 0.3.2

--- src/core/Application.php
+++ src/core/Application.php
@@ -24,7 +24,7 @@ use blink\console\ServerStopCommand;
 class Application extends ServiceLocator
 {
 
-    const VERSION = '0.3.1';
+    const VERSION = '0.3.2';
 
     /**
      * The name for the application.
