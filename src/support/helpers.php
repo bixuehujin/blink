f8cb8d4c21390f83c4a565ed7e2fcfbc7bd8eab8
Jin Hu <bixuehujin@gmail.com>
2018-06-29 23:40:26 +0800

    Enhanced env() to compatible with laravel's

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -123,7 +123,26 @@ if (!function_exists('env')) {
     {
         $value = getenv($name);
 
-        return $value !== false ? $value : $default;
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        return $value;
     }
 }
 
