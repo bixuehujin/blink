b332d904630a07ace91a240f6b619743d03a33e1
Jin Hu <bixuehujin@gmail.com>
2018-08-12 22:07:05 +0800

    Added Cors middleware

---
+++ src/http/middleware/Cors.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace blink\http\middleware;
+
+use blink\core\BaseObject;
+use blink\core\MiddlewareContract;
+
+/**
+ * Class Cors
+ *
+ * @package blink\http\middleware
+ */
+class Cors extends BaseObject implements MiddlewareContract
+{
+    public $allowOrigins = [];
+    public $allowMethods = 'GET, PUT, POST, DELETE';
+    public $allowHeaders = 'Authorization, Content-Type';
+    public $exposeHeaders = 'Authorization';
+    public $maxAge = 86400;
+
+    /**
+     * @param \blink\http\Response $response
+     */
+    public function handle($response)
+    {
+        if (request()->is('OPTIONS')) {
+            $response->data = '';
+        }
+
+        $origin = request()->headers->first('Origin');
+        if (!$origin) {
+            return;
+        }
+
+        if (!$this->matchOrigin($origin, (array)$this->allowOrigins)) {
+            return;
+        }
+
+        $response->headers->add([
+            'Vary' => 'Origin',
+            'Access-Control-Allow-Origin' => $origin,
+            'Access-Control-Allow-Methods' => $this->allowMethods,
+            'Access-Control-Allow-Headers' => $this->allowHeaders,
+            'Access-Control-Expose-Headers' => $this->exposeHeaders,
+            'Access-Control-Max-Age' => $this->maxAge,
+        ]);
+    }
+
+    protected function matchOrigin($target, $allowOrigins)
+    {
+        if (empty($allowOrigins)) {
+            return true;
+        }
+
+        $target = parse_url($target, PHP_URL_HOST);
+
+        foreach ($allowOrigins as $origin) {
+            $origin = strtr($origin, [
+                '*' => '[a-zA-Z0-9-]+',
+                '.' => '\.',
+            ]);
+
+            if (preg_match("/^$origin$/", $target)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
