9200f368432a015b8ddc8be904c4ee9553627187
Jin Hu <bixuehujin@gmail.com>
2019-07-28 16:03:55 +0800

    Bump version

--- src/core/Application.php
+++ src/core/Application.php
@@ -27,7 +27,7 @@ use Psr\Http\Message\ResponseInterface;
  */
 class Application extends ServiceLocator
 {
-    const VERSION = '0.4.0';
+    const VERSION = '0.4.1';
 
     /**
      * The name for the application.
