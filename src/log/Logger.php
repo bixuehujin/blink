2cb215a586798b5c1475dfee015ca0d9c7e5501d
Jin Hu <bixuehujin@gmail.com>
2020-05-01 22:51:22 +0800

    Extracted MonoLogger to seperated file

--- src/log/Logger.php
+++ src/log/Logger.php
@@ -4,14 +4,10 @@ namespace blink\log;
 
 use blink\core\InvalidParamException;
 use blink\core\BaseObject;
-use blink\di\Instance;
 use blink\injector\ContainerAware;
 use blink\injector\ContainerAwareTrait;
-use Monolog\Formatter\JsonFormatter;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
-use Monolog\Logger as BaseMonoLogger;
-use Monolog\Handler\HandlerInterface;
 
 /**
  * Class Logger
@@ -23,20 +19,20 @@ class Logger extends BaseObject implements LoggerInterface, ContainerAware
     use LoggerTrait;
     use ContainerAwareTrait;
 
-    public $name = 'blink';
+    public $name    = 'blink';
     public $targets = [];
 
     protected $monolog;
 
     protected $levelMap = [
         'emergency' => MonoLogger::EMERGENCY,
-        'alert' => MonoLogger::ALERT,
-        'critical' => MonoLogger::CRITICAL,
-        'error' => MonoLogger::ERROR,
-        'warning' => MonoLogger::WARNING,
-        'notice' => MonoLogger::NOTICE,
-        'info' => MonoLogger::INFO,
-        'debug' => MonoLogger::DEBUG,
+        'alert'     => MonoLogger::ALERT,
+        'critical'  => MonoLogger::CRITICAL,
+        'error'     => MonoLogger::ERROR,
+        'warning'   => MonoLogger::WARNING,
+        'notice'    => MonoLogger::NOTICE,
+        'info'      => MonoLogger::INFO,
+        'debug'     => MonoLogger::DEBUG,
     ];
 
     public function init()
@@ -60,24 +56,10 @@ class Logger extends BaseObject implements LoggerInterface, ContainerAware
     public function log($level, $message, array $context = [])
     {
         if (!isset($this->levelMap[$level])) {
-            throw new InvalidParamException('Level "'.$level.'" is not defined, use one of: '.implode(', ', array_keys($this->levelMap)));
+            throw new InvalidParamException('Level "' . $level . '" is not defined, use one of: ' . implode(', ', array_keys($this->levelMap)));
         }
 
         $this->monolog->addRecord($this->levelMap[$level], $message, $context);
     }
 }
 
-class MonoLogger extends BaseMonoLogger
-{
-    /**
-     * Hack to remove the default logger support of Monolog.
-     */
-    public function pushHandler(HandlerInterface $handler)
-    {
-        if ($handler instanceof BaseObject) {
-            array_unshift($this->handlers, $handler);
-        }
-
-        return $this;
-    }
-}
