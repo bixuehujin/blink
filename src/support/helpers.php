0e7c46c31b09fdd31595a04b9ca785bed1be7d87
Jin Hu <bixuehujin@gmail.com>
2020-05-17 00:49:23 +0800

    Added env() helper back

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -1,2 +1,36 @@
 <?php
 
+if (!function_exists('env')) {
+    /**
+     * Returns env configuration by it's name.
+     *
+     * @param string $name
+     * @param mixed $default
+     * @return mixed
+     */
+    function env($name, $default = null)
+    {
+        $value = getenv($name);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        return $value;
+    }
+}
