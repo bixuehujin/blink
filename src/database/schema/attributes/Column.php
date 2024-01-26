be0a4b08138afa2c53195a16b132f6dbaf1badc7
Jin Hu <bixuehujin@gmail.com>
2024-01-26 17:08:57 +0800

    Changed Column attribute's property

--- src/database/schema/attributes/Column.php
+++ src/database/schema/attributes/Column.php
@@ -5,17 +5,14 @@ declare(strict_types=1);
 namespace blink\database\schema\attributes;
 
 use Attribute;
+use blink\typing\Type;
 
 #[Attribute]
 class Column
 {
     public function __construct(
         public ?string $name = null,
-        public ?string $type = null,
-        public ?int $length = null,
-        public ?int $precision = null,
-        public ?int $scale = null,
-        public bool $nullable = false,
+        public ?Type $type = null,
         public mixed $default = null,
         public ?string $label = null,
         public ?string $comment = null,
